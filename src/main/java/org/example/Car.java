c366ca513ddace0d9d5e625a48039117e3eb420d
Olga Komar <komar_ol@mail.ru>
2023-08-02 14:01:04 +0300

    fourth commit

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-
 public class Car {
     List<String> nameList = Collections.emptyList();
     List<Integer> numberList= Collections.emptyList();
